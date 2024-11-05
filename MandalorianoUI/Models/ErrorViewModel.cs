0f4f92c235806846884189dac517ecb18ea1d697
amarosuarez <amaro.suarez@iesnervion.es>
2024-11-05 19:47:51 +0100

    Agregar archivos de proyecto.

---
+++ MandalorianoUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MandalorianoUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MandalorianoUI/Models/VM/clsVistaMisionesVM.cs
@@ -0,0 +1,15 @@
+﻿using MandalorianoBL;
+using MandalorianoENT;
+
+namespace MandalorianoUI.Models.VM
+{
+    public class clsVistaMisionesVM : clsMisionENT
+    {
+        public List<clsMisionENT> misiones { get; }
+
+        public clsVistaMisionesVM()
+        {
+            misiones = clsObtenerMisionesBL.obtenerMisionesBL();
+        }
+    }
+}

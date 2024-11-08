ed85e7a42ca1b1039020929e8aa6c84ceb7c5287
amarosuarez <amaro.suarez@iesnervion.es>
2024-11-08 13:30:55 +0100

    casi terminado

---
+++ MandalorianoMAUI/ViewModels/clsVistaMisiones.cs
@@ -0,0 +1,52 @@
+﻿using MandalorianoBL;
+using MandalorianoENT;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MandalorianoMAUI.ViewModels
+{
+    public class clsVistaMisiones : INotifyPropertyChanged
+    {
+        private List<clsMisionENT> misiones;
+
+        private clsMisionENT mision;
+
+        public List<clsMisionENT> Misiones { 
+            get
+            {
+                return misiones;
+            }
+        }
+
+        public clsMisionENT Mision
+        {
+            get
+            {
+                return mision;
+            }
+
+            set { 
+                mision = value;
+                NotifyPropertyChanged("Mision");
+            }
+        }
+
+        public clsVistaMisiones() { 
+            misiones = clsObtenerMisionesBL.obtenerMisionesBL();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+    }
+}
